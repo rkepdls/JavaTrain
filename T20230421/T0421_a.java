45c71f659d9c3ce3555cf75592b2de0c2a1fe817
rkepdls <rkepdls@naver.com>
2023-04-21 17:35:31 +0900

    8일차 과제

---
+++ T20230421/T0421_a.java
@@ -0,0 +1,35 @@
+package T20230421;
+
+import java.util.Scanner;
+
+public class T0421_a {
+
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+		
+		System.out.print("사람 수: ");
+		int n = stdIn.nextInt();                    //사람 수를 입력받음
+		int[] points = new int[n];                  //점수
+		
+		System.out.println("점수를 입력하세요.");
+		int sum = 0;                                //합계
+		for (int i=0; i<n; i++) {
+			System.out.print((i+1) + "번의 점수: ");
+			points[i] = stdIn.nextInt();            //points[i]를 읽는다
+			sum += points[i];                       //sum에 points[i]를 더함
+		}
+
+		int max = points[0];                        //최고점
+		int min = points[0];                        //최저점
+		for (int i=0; i<n; i++) {
+			if (points[i]>max) max = points[i];
+			if (points[i]<min) min = points[i];
+		}
+		
+		System.out.println("합계는 " + sum + "점입니다.");
+		System.out.println("평균은 " + (double)sum/n + "점입니다.");
+		System.out.println("최고점은 " + max + "점입니다.");
+		System.out.println("최저점은 " + min + "점입니다.");
+	}
+}
+//완료
